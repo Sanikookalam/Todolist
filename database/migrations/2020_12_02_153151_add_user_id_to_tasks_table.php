8783c50af8f121ed232f4baed23e2b615bc3d0dc
Sanikookalam <eagle13740@gmail.com>
2020-12-02 19:20:07 +0330

    J1 some code

--- database/migrations/2020_12_02_153151_add_user_id_to_tasks_table.php
+++ database/migrations/2020_12_02_153151_add_user_id_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+class AddUserIdToTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->boolean('done')->default(false);
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\User::class)
+                ->after('id');
         });
     }
 
@@ -28,6 +26,8 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
     }
 }
